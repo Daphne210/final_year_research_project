f8bbdd79d73ff94b228ad3e62112dae69c4a34df
Daphne210 <dapnephinerry2@gmail.com>
2025-07-18 23:51:47 +0300

    Adjusting app file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 app = Flask(__name__)
 
 # Load all antibiotic models and expected feature list
-models = joblib.load("best_xgb_models.pkl") 
+models = joblib.load("xgb_baseline_model.pkl") 
 expected_features = joblib.load("xgb_expected_features.pkl") 
 
 # HTML for file upload UI
